ea52d9f914670a1debd0a66aaef7b1d16c43bf74
Zhou <540376482yzb@gmail.com>
2018-05-02 11:24:07 -0700

    attempt to create bare-bone template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import LandingPage from './components/LandingPage'
-
-
+import LandingPage from './components/LandingPage';
+import MapWidget from './components/scratches/MapWidget';
+import { Route } from 'react-router-dom';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LandingPage/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<LandingPage />
+			</div>
+		);
+	}
 }
 
 export default App;
