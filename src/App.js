6be8dafc139dbe313fb51ee5b2117c2738d734e3
Zhou <540376482yzb@gmail.com>
2018-05-02 12:12:59 -0700

    bare-bone

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import LandingPage from './components/LandingPage';
 import { Route } from 'react-router-dom';
 import SignUpPage from './components/Signup';
 import Login from './components/Login';
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Header />
-				<LandingPage />
+				<Route exact path="/" component={LandingPage} />
+				<Route exact path="/dashboard" component={Dashboard} />
 			</div>
 		);
 	}
-
+}
 export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function LandingPage(props) {
+	return <div>this is landing page</div>;
+}
