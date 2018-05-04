680acaf777b146034a933796467d8978ff47912f
Evan Garrett <evang522@gmail.com>
2018-05-04 12:15:52 -0400

    Organize reducers, connect eventlist to State, create actions for populating events

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -3,36 +3,22 @@ import React from 'react';
 import {connect} from 'react-redux';
 import EventListEvent from './EventListEvent';
 import './styles/EventList.css'
+import {fetchUserEvents} from '../store/actions/eventlist.actions';
 //================================== Event List Component ====================>
 /**
  * Responsible for Containing a list of Events belonging to the user, conditionally displaying upcoming events or past events.
  * The Events listed will be individual Event components themselves. 
  */
 export class EventList extends React.Component {
-  constructor(props) {
-
-    super(props)
-
-    //some Mock data. This can be deleted. Also, the styles I provided are totally temporary and should be deleted.
-    this.events = [
-      {
-        title:'Beach Volleyball',
-        starttime: "Tomorrow at 5 PM",
-        location:'Melbourne Florida'
-      },
-      {
-        title:'NodeSchool Meetup',
-        starttime: "Tomorrow at 8PM",
-        location: 'Miami, Florida'
-      }
-    ]
-
-  }
   
+  componentDidMount() {
+   this.props.dispatch(fetchUserEvents());
+  }
+
   render() {
 
   // This code should be changed to loop over this.PROPS.events, not this.events. 
-  const events = this.events ? this.events.map((event,index) => <EventListEvent event={event} key={index} /> ) : '';
+  const events = this.props.events ? this.props.events.map((event,index) => <EventListEvent event={event} key={index} /> ) : '';
 
   /*
   Regarding the question of how to display upcoming events vs past events. I would first argue that an event should be `upcoming` until it is `past`. For an MVP we
@@ -42,7 +28,7 @@ export class EventList extends React.Component {
   past events, or the current event. If the user is viewing the upcoming events, we can display all events with a startdate that is after the current moment. If the user is viewing past 
   events, we can display events with a starttime that is less than the current moment's starttime value. 
   */
-  
+    console.log(this.props, 'EVENTS');
     return(
       <div className='event-list-container'>
         <div className='event-list-header'>
@@ -57,8 +43,11 @@ export class EventList extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
-  events:state.events
-});
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    events:state.events.eventList
+  }
+};
 
 export default connect(mapStateToProps)(EventList);
---
+++ src/store/reducers/events.reducers.js
@@ -0,0 +1,49 @@
+import { CREATE_EVENT } from '../actions/eventcreation';
+import { STORE_EVENTLIST } from '../actions/eventlist.actions';
+
+const initialState = {
+	activeEvent: {
+		id: null,
+		location: null,
+		lat: null,
+		long: null,
+		starttime: null,
+		title: null,
+		widgets: null
+	},
+	// Eventlist: a list of events that belong to the user. Retrieved from the DB in order to list upcoming and past events.
+	eventList:null
+	/*======= other props waiting to fill up
+    state related to events goes below
+  */
+};
+   
+export default function eventReducer(state = initialState, action) {
+	switch(action.type) {
+		
+		//---------------
+		case CREATE_EVENT:
+		return {
+			...state, activeEvent:action.event
+		}
+		
+		//----------------
+		case STORE_EVENTLIST:
+		console.log('store eventlist reducer was hit');
+		return {
+			...state,
+			eventList:action.events
+		}
+
+		//----------------
+		default:
+		return {
+			state
+		}
+
+		//--------------
+	}
+}
+
+
+
