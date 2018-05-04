935ff5d55f26e42803ea5a7aee8d0f359b9bba6e
Evan Garrett <evang522@gmail.com>
2018-05-04 13:11:54 -0400

    Remove cats and dogs

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,3 @@
-export {getDogs, deleteDog} from './dog'
-export {getCats, deleteCat} from './cat'
 export {fetchCoordinates} from './googleGeo';
 export {registerUser} from './users'
 export {login, refreshAuthToken} from './auth'
