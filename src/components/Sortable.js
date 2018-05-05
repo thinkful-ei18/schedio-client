9f429081beb937ab052dda345bbfd3b981e6221c
mattNat <mattsanpedro76@gmail.com>
2018-05-04 17:16:19 -0700

    active event is the earliest event by default && add list drag and drop component

--- src/components/Sortable.js
+++ src/components/Sortable.js
@@ -29,20 +29,44 @@ export class SortableComponent extends Component {
   state = {
     items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
   };
+
   onSortEnd = ({oldIndex, newIndex}) => {
     this.setState({
       items: arrayMove(this.state.items, oldIndex, newIndex),
     });
   };
 
+  earlyEvent = (events) => {
+    let earlyEventTime = new Date('3000-01-01T08:00:00.000Z');
+    let earlyEvent;
+    for (let i=0; i<events.length; i++) {
+      const currentEvent = new Date(events[i].starttime)
+      if (currentEvent < earlyEventTime) {
+        earlyEventTime = currentEvent;
+        earlyEvent = events[i];
+      }
+    }
+
+    let data = {};
+    for (let k in earlyEvent) {
+      if (earlyEvent.hasOwnProperty(k) && k === 'location') {
+        data = earlyEvent[k];
+      }
+    }
+    return data;
+  }
+
   
   render() {
-    let events = this.props.events ? this.props.events.map((event,index) => console.log(index, event) ) : '';
-    console.log(events, 'FROM SORTABLE');
-    
+    let events = this.props.events ? this.props.events : '';
+    let currentEvent = this.earlyEvent(events);
+    console.log(currentEvent);
+
     return (
       <div>
         <div className='left'>
+          <h4>Active (Earliest Event): {currentEvent.address}</h4>
+          <h5>Latitude: {currentEvent.lat} Longitude: {currentEvent.lng}</h5>          
           <SortableList 
             items={this.state.items} 
             onSortEnd={this.onSortEnd} 
