d241078b7fb551964c74fbfd69b6f30120855eb6
Zhou <540376482yzb@gmail.com>
2018-05-07 08:23:52 -0700

    fix

--- src/components/EventView.js
+++ src/components/EventView.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {connect} from 'react-redux';
-// import {Moment} from 'react-moment';
-// import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
-// import FlatButton from 'material-ui/FlatButton';
+import { connect } from 'react-redux';
+import { Moment } from 'react-moment';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import moment from 'moment';
 
 export class EventView extends React.Component {
-
   render() {
-
     return (
       <Card>
-        {this.props.currentEvent ? 
+        {this.props.currentEvent ? (
           <div>
             <CardHeader
               title={this.props.currentEvent.title}
@@ -19,12 +17,13 @@ export class EventView extends React.Component {
               showExpandableButton={false}
             />
             <CardText expandable={false}>
-              {this.props.currentEvent.address ? this.props.currentEvent.address : ''} 
-              {this.props.currentEvent? moment(this.props.currentEvent.starttime).fromNow() : ''}
-            </CardText> 
-          </div>: 'No Event Selected'
-        }
-       
+              {this.props.currentEvent.address ? this.props.currentEvent.address : ''}
+              {this.props.currentEvent ? moment(this.props.currentEvent.starttime).fromNow() : ''}
+            </CardText>
+          </div>
+        ) : (
+          'No Event Selected'
+        )}
       </Card>
     );
   }
---
+++ src/store/actions/widgetAction/mapWidget.action.js
@@ -0,0 +1,16 @@
+import { API_BASE_URL } from '../../../config';
+import { normalizeResponseErrors } from '../utils';
+import {normalizeResponseErrors} from '../utils.js'
+export const displayMapWidget = id => (dispatch, getState) => {
+  return fetch(`${API_BASE_URL}/events/${id}/mapWidget`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${getState().auth.authToken}`
+    },
+    body:JSON.stringify()
+  })
+  .then(res => normalizeResponseErrors(res))
+  .then(res => res.json())
+  .then(event => )
+};
