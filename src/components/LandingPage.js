6fd5642d9abd22ab32138d8ee3b70f046db8e23d
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-05-09 13:44:37 -0400

    ui exp

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,27 +1,37 @@
 import React from 'react';
+import { zoomInRight } from 'react-animations';
+import Radium, { StyleRoot } from 'radium';
 import './LandingPage.css';
+
+const styles = {
+  zoomInRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomInRight, 'zoomInRight')
+  }
+};
+
 export default function LandingPage(props) {
   return (
     <div className="container">
       <h1> Schedio</h1>
-      <div className="info-progression">
-        <div className="f-pg">
-          <i className="fa fa-book fa-5x"></i>
-          <p>Plan</p>
-        </div>
-   
-        <div className="f-pg">
-          <i className="fa fa-paper-plane fa-5x">
-          </i>
-          <p>Schedule</p>
-        </div>
+      <StyleRoot>
+        <div className="info-progression" style={styles.zoomInRight}>
+          <div className="f-pg">
+            <i className="fa fa-book fa-5x" />
+            <p>Plan</p>
+          </div>
 
-        <div className="f-pg">
-          <i className="fa fa-calendar fa-5x">
-          </i>
-          <p>Commit</p>
+          <div className="f-pg" style={styles.zoomInRight}>
+            <i className="fa fa-paper-plane fa-5x" />
+            <p>Schedule</p>
+          </div>
+
+          <div className="f-pg" style={styles.zoomInRight}>
+            <i className="fa fa-calendar fa-5x" />
+            <p>Commit</p>
+          </div>
         </div>
-      </div>
+      </StyleRoot>
     </div>
   );
 }
