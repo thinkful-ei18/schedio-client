e67cbbb33d0d63128e7b45dd27970e6b2423ec59
mattNat <mattsanpedro76@gmail.com>
2018-05-16 14:23:47 -0700

    finish instructions for weather, map, hiking, and food widgets

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -40,6 +40,11 @@ export default function LandingPage(props) {
                 nearby restaurants, hiking trails, weather forecast, and sporting events 
                 all in one app.  Plan, schedule, and commit!
             </p>
+            <h2>Demo the App</h2>            
+            <p>
+              Credentials have been created to demo the app.  Simply go to login page and type in "demo"
+              for username and "demo" for password.
+            </p>
             <h2>Registration and Login Instructions</h2>
             <p>
               If you do not have a user account, please register by first clicking on the 
@@ -98,6 +103,65 @@ export default function LandingPage(props) {
                 things to remember widgets.
               </li>
             </ol>
+            <h2>&lt;INSERT PICTURE WIDGET CONFIGURATION BUTTON&gt;</h2>                          
+            <h2>Displaying Widgets for Current Event</h2>                          
+            <p>
+              To add widgets to the event currently on the "View Event" tab, first click on the 
+              "gear" on the top right.  A window will pop up and display all available widgets.  
+              For basic events, notice the Weather and Checklist widgets have been turned on by default.
+              Turn on or off the widgets with the switch buttons on the right-hand-side.
+              When done, click on the "Confirm" button on the bottom.
+            </p>
+            <h2>&lt;INSERT PICTURE WIDGET CONFIGURATION WINDOW&gt;</h2>
+            <h2>Weather Widget Instructions</h2>                          
+            <h2>&lt;INSERT PICTURE WEATHER WIDGET&gt;</h2>            
+            <p>
+              The Weather widget is based on the event location and date.  It displays 
+              the forecast of the event date if within a seven day time frame.  If the event date is more than 
+              seven days away, the Weather widget will default to the forecast of current date.
+            </p>
+            <h2>Map Widget Instructions</h2>                          
+            <h2>&lt;INSERT PICTURE MAP WIDGET&gt;</h2>            
+            <p>
+              The Map widget is based on the event location and Google Maps.  You can use
+              Google maps functionalities including zooming in and out, map and satellite displays, and 
+              toggle full screen view.  In addition, the location displayed on the map can be changed by
+              first clicking on the gear icon on the top right of thee widget.
+            </p>
+            <h2>&lt;INSERT PICTURE MAP WIDGET CHANGE SETTINGS&gt;</h2>            
+            <p>
+              The Map widget settings allows you to change the widget title and location being 
+              displayed.  Input the desired title in the "widget title" input.  To change the location,
+              input the address or general location and click on the destination on the dropdown menu.
+              Once done, click on "SHOW MAP" to proceed.
+            </p>
+            <h2>&lt;INSERT PICTURE MAP WIDGET CHANGE SETTINGS CONFIRM&gt;</h2>            
+            <p>
+              Click "Confirm" to save the new title and location on widget.
+            </p>
+            <h2>Hiking Widget Instructions</h2>                          
+            <h2>&lt;INSERT PICTURE HIKING WIDGET BEFORE&gt;</h2>            
+            <p>
+              The Hiking widget return one of ten most popular hiking trails in the desired location.
+              Begin by typing in the location in the search input box.  Click on the correct destination
+              on the dropdown list.
+            </p>
+            <h2>&lt;INSERT PICTURE HIKING WIDGET AFTER&gt;</h2>            
+            <p>
+              After clicking on the desired location, one of the top ten hiking spots appears on the widget.
+            </p>
+            <h2>Food Widget Instructions</h2>                          
+            <h2>&lt;INSERT PICTURE FOOD WIDGET BEFORE&gt;</h2>            
+            <p>
+              The Food widget uses the core functionality of the yelp API to return the best rated restaurants
+              submitted in the search window.  After submitting a search, you are redirected to a results page.
+            </p>
+            <h2>&lt;INSERT PICTURE FOOD WIDGET AFTER&gt;</h2>            
+            <p>
+             To display find more information about the restaurant, click on the picture icon that
+             conatins a link to the yelp reviews, pictures, menu, and address.  Once the restaurant decision
+             has been made, click "CHOOSE" and the restarant is saved on the Event View display.
+            </p>
           </div>
         </div>
       </StyleRoot>
