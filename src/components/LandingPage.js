ad0d845b061e9c881249bd75b41f1caf9553fae6
mattNat <mattsanpedro76@gmail.com>
2018-05-16 12:48:17 -0700

    finish event creation instructions

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -32,7 +32,7 @@ export default function LandingPage(props) {
           </div>
 
           <div className="instructions">
-            <h2>"INSERT PICTURE CAROUSEL HERE"</h2>
+            <h2>&lt;INSERT PICTURE CAROUSEL HERE&gt;</h2>
             <h2>What is Schedio?</h2>            
             <p>
               Schedio is the Greek word for design.  We give users the freedom to design
@@ -50,22 +50,54 @@ export default function LandingPage(props) {
               clicking on the "Login With Google" button.  This will prompt and pop up
               window to provide Google account credentials. 
             </p>
-            <h2>"INSERT PICTURE REGISTRATION HERE"</h2>
-            <h2>"Dashboard Instructions"</h2>            
+            <h2>&lt;INSERT PICTURE REGISTRATION HERE&gt;</h2>
+            <h2>Dashboard Instructions</h2>            
             <p>
               On successful login, user is taken to the dashboard page.  The top left 
               navigation icon allows nagivation to the home page to access instructions 
               and the current dashboard page.
             </p>
-            <h2>"INSERT PICTURE LOGIN NAVIGATION HERE"</h2>
+            <h2>&lt;INSERT PICTURE LOGIN NAVIGATION HERE&gt;</h2>
             <p>
               The Events window contains three subsection including View Event, 
               Upcoming Event, and Past Events.  By default, View Event is active on the
               display window.  The current date is calculated and associates upcoming and 
-              past events in their respective tabs.  
+              past events in their respective tabs.  To add an event, click on the red "+" 
+              tab found at the bottom of each tab.
             </p>
-            <h2>"INSERT PICTURE EVENT TABS HERE"</h2>
-            
+            <h2>&lt;INSERT PICTURE EVENT TABS HERE&gt;</h2>
+            <h2>Add Event Instructions</h2>            
+            <p>
+              After clicking the red "+" icon on the bottom of an Event tab, a window pops
+              up, prompting a few inputs.  
+            </p>
+            <ol>
+              <h2>&lt;INSERT PICTURE EVENT CREATION STEP 1 HERE&gt;</h2>            
+              <li>
+                Select the date by clicking on the calendar.  The year can be cycled
+                with the "&lt;&lt;" and "&gt;&gt;" buttons on the top corners.  The month can be cyled through
+                with the "&lt;" and "&gt;" buttons on top.  Click "Next" when date has been selected.
+              </li>
+              <h2>&lt;INSERT PICTURE EVENT CREATION STEP 2 HERE&gt;</h2>                          
+              <li>
+                The second steps involves setting up a time for the event.  First, set the hour with the
+                clock widget.  Then, the set the minute for the start time.  If the hour or minute is
+                already set to desired time, press "OK" on the bottom right.  Once the time has been set,
+                click "NEXT" to proceed.
+              </li>
+              <h2>&lt;INSERT PICTURE EVENT CREATION STEP 3 HERE&gt;</h2>                          
+              <li>
+                Enter the address, national park, or city/town into the "Search Places" input box.
+                After typing in the first few characters, Google search pulls down a list of likely
+                candidates.  Click your desired location when it appears, then click "Next."
+              </li>
+              <h2>&lt;INSERT PICTURE EVENT CREATION STEP 4 HERE&gt;</h2>                          
+              <li>
+                The last step of the event creation process includes selecting which type of event
+                is being planned.  Selecting "Basic" as a default to load the weather and 
+                things to remember widgets.
+              </li>
+            </ol>
           </div>
         </div>
       </StyleRoot>
